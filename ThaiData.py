946dff3335064f3feb60ff2f35cdf4276869fff3
Song Li <songl@outlook.com>
2016-11-16 18:44:42 +0800

    Add more characters

--- ThaiData.py
+++ ThaiData.py
@@ -1,6 +1,15 @@
 Consonants = []
-
 for i in range(3585, 3630 + 1):
     Consonants.append(chr(i))
 
-print(Consonants)
+Vowels = []
+for i in range(3632, 3642 + 1):
+    Vowels.append(chr(i))
+for i in range(3648, 3654 + 1):
+    Vowels.append(chr(i))
+
+Tones = []
+for i in range(3655, 3660 + 1):
+    Tones.append(chr(i))
+
+MarkEnd = [chr(3631), chr(3674)]
